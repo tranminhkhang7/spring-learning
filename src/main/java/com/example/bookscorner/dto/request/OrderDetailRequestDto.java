417f5060c87dfa01fced60bc6acf1f350b1a8dac
tranminhkhang <tranminhkhang7@gmail.com>
2022-11-09 03:21:11 +0700

    Unit test (not working yet), order books

---
+++ src/main/java/com/example/bookscorner/dto/request/OrderDetailRequestDto.java
@@ -0,0 +1,2 @@
+package com.example.bookscorner.dto.request;public class OrderDetailDto {
+}
---
+++ src/main/java/com/example/bookscorner/repositories/OrderDetailRepository.java
@@ -0,0 +1,2 @@
+package com.example.bookscorner.repositories;public interface OrderDetailRepository {
+}
